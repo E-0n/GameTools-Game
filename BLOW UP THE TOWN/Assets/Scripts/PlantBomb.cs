75356187c71719713a6890347d2f98d871038531
E-0n <c18308816@mydit.ie>
2020-05-12 17:13:44 +0100

    Did some stuff

---
+++ BLOW UP THE TOWN/Assets/Scripts/PlantBomb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantBomb : MonoBehaviour
+{
+    public float throwForce = 1.5f;
+    public GameObject bombPrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlaceBomb();
+        }
+    }
+
+    void PlaceBomb()
+    {
+        Instantiate(bombPrefab, transform.position, transform.rotation);
+        Rigidbody rb = bombPrefab.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+    }
+}
---
+++ BLOW UP THE TOWN/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController thePlayer;
+
+    public float speed = 12f;
+    public float gravity = -9.61f;
+    public float jumpHeight = 3f;
+
+    private Vector3 move;
+    private Camera _camera;
+
+    private void OnEnable()
+    {
+        _camera = Camera.main;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        float oldY = move.y;
+
+        Vector3 direction = new Vector3(x, 0, y);
+
+        move = _camera.transform.TransformDirection(direction);
+        move *= speed;
+        move.y = oldY;
+
+        if (Input.GetButtonDown("Jump") && thePlayer.isGrounded)
+        {
+            move.y = jumpHeight;
+        }
+
+        if (!thePlayer.isGrounded)
+        {
+            move.y += gravity * Time.deltaTime;
+        }
+
+        thePlayer.Move(move * Time.deltaTime);
+    }
+}
