185f516e4948f027b9e015a835810504584a664d
E-0n <c18308816@mydit.ie>
2020-05-27 00:00:19 +0100

    FINISHED MAKING IT
    Added a fix to placing the bombs
    Made the bombs explode
    Added a bomb limit

--- BLOW UP THE TOWN/Assets/Scripts/PlantBomb.cs
+++ BLOW UP THE TOWN/Assets/Scripts/PlantBomb.cs
@@ -6,19 +6,41 @@ public class PlantBomb : MonoBehaviour
 {
     public float throwForce = 1.5f;
     public GameObject bombPrefab;
+    public int amount = 4;
+    private int _currentAmount;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && _currentAmount < amount)
         {
             PlaceBomb();
+            _currentAmount++;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Explode();
         }
     }
 
     void PlaceBomb()
     {
-        Instantiate(bombPrefab, transform.position, transform.rotation);
-        Rigidbody rb = bombPrefab.GetComponent<Rigidbody>();
-        rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+        GameObject bomb = Instantiate(bombPrefab, transform.position, Quaternion.LookRotation(Camera.main.transform.forward.normalized));
+        Rigidbody rb = bomb.GetComponent<Rigidbody>();
+        rb.AddForce(Camera.main.transform.forward.normalized * throwForce, ForceMode.Impulse);
+    }
+
+    void Explode()
+    {
+        foreach (GameObject b in GameObject.FindGameObjectsWithTag("BOMBY"))
+        {
+            CapsuleCollider boom = b.GetComponent<CapsuleCollider>();
+
+            if (boom)
+            {
+                boom.enabled = true;
+                Destroy(b, 0.1f);
+            }
+        }
     }
 }
